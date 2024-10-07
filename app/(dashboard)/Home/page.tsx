dfee3658b8efdf4b96881939f8d33614856d237f
pro1er <orgilpro0@gmail.com>
2024-10-07 16:52:50 +0800

    mobile

--- app/(dashboard)/Home/page.tsx
+++ app/(dashboard)/Home/page.tsx
@@ -1,20 +1,26 @@
-import AboutUs from '@/components/pages/AboutUs/AboutUs'
-import Certificate from '@/components/pages/Certificate/Certificate'
-import Contact from '@/components/pages/Contact/Contact'
-import Features from '@/components/pages/Features/Features'
-import HomeHero from '@/components/pages/Home/HomeHero'
-import ProductGrid from '@/components/pages/Products/Products'
-import React from 'react'
+import AboutUs from "@/components/pages/AboutUs/AboutUs";
+import Certificate from "@/components/pages/Certificate/Certificate";
+import Contact from "@/components/pages/Contact/Contact";
+import Features from "@/components/pages/Features/Features";
+import HomeHero from "@/components/pages/Home/HomeHero";
+import ProductGrid from "@/components/pages/Products/Products";
+import React from "react";
 
 export default function page() {
   return (
     <div>
-      <HomeHero/>
-      <AboutUs/>
-      <Certificate/>
-      <Features/>
-      <ProductGrid/>
-      <Contact/>
+      <div className="hidden md:block">
+        <HomeHero />
+        <AboutUs />
+        <Certificate />
+        <Features />
+        <ProductGrid />
+        <Contact />
+      </div>
+      <div className="md:hidden flex justify-center items-center h-screen w-full flex-col">
+          <h1 className=" font-bold">USE DESKTOP</h1>
+          <p>Under Development</p>
+      </div>
     </div>
-  )
+  );
 }

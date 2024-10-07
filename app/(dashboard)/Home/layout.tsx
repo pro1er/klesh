3cae1a1a3f6b404506d914bdd10f2791c5ceb361
pro1er <orgilpro0@gmail.com>
2024-10-07 16:18:26 +0800

    4th commit

--- app/(dashboard)/Home/layout.tsx
+++ app/(dashboard)/Home/layout.tsx
@@ -5,9 +5,7 @@ export default function Homelayout({
 }) {
   return (
     <div>
-      <div className="min-[1920px]:px-[200px] min-[2400px]:px-[400px]  min-[3000px]:px-[700px]">
         <main>{children}</main>
-      </div>
     </div>
   );
 }

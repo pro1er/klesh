4704b837ae9c3e42512d6ab8b4b584df340b114a
pro1er <orgilpro0@gmail.com>
2024-10-07 16:23:25 +0800

    5th push

--- app/(dashboard)/Home/layout.tsx
+++ app/(dashboard)/Home/layout.tsx
@@ -5,7 +5,9 @@ export default function Homelayout({
 }) {
   return (
     <div>
+      <div className="min-[1920px]:px-[200px] min-[2400px]:px-[400px]  min-[3000px]:px-[700px]">
         <main>{children}</main>
+      </div>
     </div>
   );
 }

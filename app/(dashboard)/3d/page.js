5edde93c16b05a2b168845329a9310614a7f2d7f
pro1er <orgilpro0@gmail.com>
2024-11-11 15:39:39 +0800

    3d

---
+++ app/(dashboard)/3d/page.js
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react';
+import ThreeCanvas from './ThreeCanvas';
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <ThreeCanvas />
+    </div>
+  );
+}

89bac8d8e6291522fd6782a7ce996d84e73a0858
pro1er <orgilpro0@gmail.com>
2025-01-14 13:37:26 +0800

    iphone fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+const nextConfig = {
+  async redirects() {
+      return [];
+  },
+};
+
 export default withNextIntl(nextConfig);
